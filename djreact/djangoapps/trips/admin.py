6379a91f46de228c5abfacc4e85cbd36ca59b7a0
Awais Jibran <awaisdar001@gmail.com>
2019-04-16 14:17:32 +0500

    Update Trips Modal and Modal Admin
    This PR makes the following changes.
    1. cancelation_policy moved from trip to hostel modal.
    2. Adjusted the `max_length` of fields.
    3. Renamed `cancelation_policy` field in trip modal to `_cancelation_policy`.
    4. Added property in trip modal `cancelation_policy`
    5. Added Meta classes of modals with Ordering and `verbose_name_plural`
    6. Added Unicode modal methods.
    7. Updated modal admins to use filter/search/list display and inline modal admins.

--- djreact/djangoapps/trips/admin.py
+++ djreact/djangoapps/trips/admin.py
@@ -3,14 +3,84 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from models import (
-    Trip, Activity, Facility, Location, TripItinerary, TripSchedule, Host
-)
+from models import (Activity, Facility, Host, Location, Trip, TripItinerary,
+                    TripSchedule)
 
-admin.site.register(Trip)
-admin.site.register(Activity)
-admin.site.register(Facility)
-admin.site.register(Location)
-admin.site.register(TripItinerary)
-admin.site.register(TripSchedule)
-admin.site.register(Host)
+
+class TripScheduleAdminInline(admin.TabularInline):
+    """Trip schedule inline modal admin"""
+    model = TripSchedule
+    extra = 0
+
+
+class TripItineraryAdminInline(admin.StackedInline):
+    """Trip itinerary inline modal admin"""
+    model = TripItinerary
+    extra = 0
+
+
+class TripAdmin(admin.ModelAdmin):
+    """Trip modal admin configuration"""
+    inlines = [TripItineraryAdminInline, TripScheduleAdminInline]
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = (
+        'name', 'slug', 'price'
+    )
+    list_filter = ('starting_location',)
+    search_fields = ['name', 'description', 'locations_included']
+
+
+class ActivityAdmin(admin.ModelAdmin):
+    """Activity modal admin configuration"""
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = (
+        'name', 'slug',
+    )
+
+
+class FacilityAdmin(admin.ModelAdmin):
+    """Facility modal admin configuration"""
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = (
+        'name', 'slug',
+    )
+
+
+class LocationAdmin(admin.ModelAdmin):
+    """Location modal admin configuration"""
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = (
+        'name', 'slug',
+    )
+
+
+class TripItineraryAdmin(admin.ModelAdmin):
+    """Trip itinerary modal admin configuration"""
+    list_display = ('trip', 'description')
+    list_filter = ('trip',)
+    search_fields = ['trip']
+
+
+class TripScheduleAdmin(admin.ModelAdmin):
+    """Trip schedule admin configuration"""
+    list_display = ('trip', 'date_from')
+    list_filter = ('date_from',)
+    search_fields = ['trip']
+
+
+class HostAdmin(admin.ModelAdmin):
+    """Host modal admin configuration"""
+    list_display = (
+        'name', 'description', 'verified'
+    )
+    list_filter = ('verified',)
+    search_fields = ['name', 'description']
+
+
+admin.site.register(Trip, TripAdmin)
+admin.site.register(Activity, ActivityAdmin)
+admin.site.register(Facility, FacilityAdmin)
+admin.site.register(Location, LocationAdmin)
+admin.site.register(TripItinerary, TripItineraryAdmin)
+admin.site.register(TripSchedule, TripScheduleAdmin)
+admin.site.register(Host, HostAdmin)
